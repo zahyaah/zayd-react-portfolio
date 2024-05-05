1e5272a1a2d3a15a9237095d842daf1e18359c85
zahyaah <itszayd2003@gmail.com>
2024-05-05 14:54:32 +0530

    fix bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,11 @@ import Card from "./components/Card/Card.jsx"
 function App() {
     return (
         <div className="app-container">
-            <NavigationBar />
-            <div className="background-image">
-                <br/>
-                <br/>
-                <Card head="Languages"
-                      values={["HTML5", "Java", "CSS3", "Python", "JavaScript", "C++", "Bash", "SQL"]}/>
+            <NavigationBar/>
+            <Card head="Languages"
+                  values={["HTML5", "Java", "CSS3", "Python", "JavaScript", "C++", "Bash", "SQL"]}/>
 
-            </div>
+
         </div>
     )
 }
