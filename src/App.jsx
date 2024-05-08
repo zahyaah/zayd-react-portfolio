2bdc689867a504bd17c0c1924302f857d4181a29
zahyaah <itszayd2003@gmail.com>
2024-05-09 00:20:37 +0530

    add footer in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import NavigationBar from "./components/NavigationBar/NavigationBar.jsx"
 import Card from "./components/Card/Card.jsx"
-
+import Footer from "./components/Footer/Footer.jsx"
 
 function App() {
     return (
+
         <div className="app-container">
             <NavigationBar/>
             <Card head="Languages"
                   values={["HTML5", "Java", "CSS3", "Python", "JavaScript", "C++", "Bash", "SQL"]}/>
 
 
+            <Footer/>
         </div>
+
     )
 }
 
